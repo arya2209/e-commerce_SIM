e30364107e06683c947262bd73acaa3746860965
arya <dzakiarya222@gmail.com>
2025-05-28 10:48:19 +0700

    register balaa

---
+++ config/login-regis.js
@@ -0,0 +1,23 @@
+document.getElementById('loginForm').addEventListener('submit', function (e) {
+    e.preventDefault();
+    const user = document.getElementById('loginUsername').value;
+    const pass = document.getElementById('loginPassword').value;
+    if (user && pass) {
+        alert(`Welcome back, ${user}!`);
+    } else {
+        alert("Please fill in all fields.");
+    }
+});
+
+function togglePassword(id, icon) {
+    const input = document.getElementById(id);
+    if (input.type === "password") {
+        input.type = "text";
+        icon.classList.remove("fa-eye");
+        icon.classList.add("fa-eye-slash");
+    } else {
+        input.type = "password";
+        icon.classList.remove("fa-eye-slash");
+        icon.classList.add("fa-eye");
+    }
+}
